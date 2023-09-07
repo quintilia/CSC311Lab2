e9a57ed72f884172a7992f671dfeab62b7d685e9
quintilia <parnm@farmingdale.edu>
2023-09-07 13:15:30 -0400

    Lab2

---
+++ src/main/java/com/example/csc311_gui_basics/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.csc311_gui_basics;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Label welcomeText1;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onNewButtonClick() {
+        welcomeText.setText("Mmmmmmm!");
+    }
+}
